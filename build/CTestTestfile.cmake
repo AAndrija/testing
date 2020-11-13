eb99f33e0dd8c5c2a0b7ffe0f4827ec1c0cba6fe
AAndrija <aa51709@fer.hr>
2020-11-13 23:52:10 +0100

    init commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/andrija/Desktop/pink_mapper/src
+# Build directory: /home/andrija/Desktop/pink_mapper/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
